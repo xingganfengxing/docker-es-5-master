178cfaa78f8b99da5eeec99b460488425b3b65e0
guozhiwei <294132756@qq.com>
2017-12-05 18:23:28 +0800

    modified:   Dockerfile 	modified:   docker-entrypoint.sh

--- 5.6.3/docker-entrypoint.sh
+++ 5.6.3/docker-entrypoint.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
+ES_HOME=/usr/share/elasticsearch
+CMD="$ES_HOME/bin/elasticsearch"
+DEFAULT_ES_USER=elasticsearch
 
-set -e
-
-# Add elasticsearch as command if needed
-if [ "${1:0:1}" = '-' ]; then
-	set -- elasticsearch "$@"
-fi
-
-# Drop root privileges if we are running elasticsearch
-# allow the container to be started with `--user`
-if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
-	# Change the ownership of user-mutable directories to elasticsearch
-	for path in \
+if [ `id -u` = 0 ]; then
+  for path in \
 		/usr/share/elasticsearch/data \
 		/usr/share/elasticsearch/logs \
 	; do
-		chown -R elasticsearch:elasticsearch "$path"
+		chown -R  elasticsearch:elasticsearch "$path"
 	done
-	
-	set -- gosu elasticsearch "$@"
-	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
+  su -c "$CMD" $DEFAULT_ES_USER
+  echo "Running as non-root..."
+else
+  $CMD
 fi
-
-# As argument is not related to elasticsearch,
-# then assume that user wants to run his own process,
-# for example a `bash` shell to explore this image
-exec "$@"
