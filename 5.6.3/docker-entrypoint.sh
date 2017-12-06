8360a468a97c1fb0d47516bdb8f769cf0136f766
guozhiwei <294132756@qq.com>
2017-12-06 11:25:16 +0800

    modified:   Dockerfile 	modified:   docker-entrypoint.sh

--- 5.6.3/docker-entrypoint.sh
+++ 5.6.3/docker-entrypoint.sh
@@ -3,6 +3,12 @@ ES_HOME=/usr/share/elasticsearch
 CMD="$ES_HOME/bin/elasticsearch"
 DEFAULT_ES_USER=elasticsearch
 
+export J2SDKDIR=/usr/lib/jvm/oracle_jdk8
+export J2REDIR=/usr/lib/jvm/oracle_jdk8/jre
+export PATH=$PATH:/usr/lib/jvm/oracle_jdk8/bin:/usr/lib/jvm/oracle_jdk8/db/bin:/usr/lib/jvm/oracle_jdk8/jre/bin
+export JAVA_HOME=/usr/lib/jvm/oracle_jdk8
+export DERBY_HOME=/usr/lib/jvm/oracle_jdk8/db
+
 if [ `id -u` = 0 ]; then
   for path in \
 		/usr/share/elasticsearch/data \
