4b3bf0b6c0547638f12b0629605743235acf7f63
guozhiwei <294132756@qq.com>
2017-12-06 18:44:32 +0800

    dd

--- centos/5.6.3/docker-entrypoint.sh
+++ centos/5.6.3/docker-entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 ES_HOME=/usr/local/elasticsearch
 CMD="$ES_HOME/bin/elasticsearch"
-DEFAULT_ES_USER=elasticsearch
+DEFAULT_ES_USER=search
 
 if [ `id -u` = 0 ]; then
   for path in \
+  	/usr/local/elasticsearch/config \
 		/usr/local/elasticsearch/data \
 		/usr/local/elasticsearch/logs \
 	; do
-		chown -R  elasticsearch:elasticsearch "$path"
+		chown -R  search:search "$path"
 	done
   su -c "$CMD" $DEFAULT_ES_USER
   echo "Running as non-root..."
