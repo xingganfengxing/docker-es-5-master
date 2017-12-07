005a20f7e1ee63ff4ba7571a95e0e6d4f2f1c92e
guozhiwei <294132756@qq.com>
2017-12-07 09:41:09 +0800

    修改为了Root可执行

--- centos/5.6.3/docker-entrypoint.sh
+++ centos/5.6.3/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ES_HOME=/usr/local/elasticsearch
-CMD="$ES_HOME/bin/elasticsearch"
+CMD="$ES_HOME/bin/elasticsearch -Des.insecure.allow.root=true"
 DEFAULT_ES_USER=search
 
 if [ `id -u` = 0 ]; then
@@ -9,7 +9,8 @@ if [ `id -u` = 0 ]; then
 		/usr/local/elasticsearch/data \
 		/usr/local/elasticsearch/logs \
 	; do
-		chown -R  search:search "$path"
+		#chown -R  search:search "$path"
+		$CMD
 	done
   su -c "$CMD" $DEFAULT_ES_USER
   echo "Running as non-root..."
